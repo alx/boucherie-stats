4ccdf4cd85e4aec31f0cb79f3ae324cbcf9c3d58
Alexandre Girard <git@alexgirard.com>
2012-06-08 11:49:48 +0200

    finish html/js mockup with necessary js lib: morris for graph and sticky for offline DB storage

--- javascripts/app.js
+++ javascripts/app.js
@@ -95,4 +95,21 @@ jQuery(document).ready(function ($) {
 	/* DISABLED BUTTONS ------------- */
 	/* Gives elements with a class of 'disabled' a return: false; */
   
+  Morris.Line({
+  element: 'plot',
+  data: [
+    {q: '2009 Q3', a: 100, b: 75},
+    {q: '2010 Q2', a: 75, b: 50},
+    {q: '2010 Q3', a: 75, b: 50},
+    {q: '2011 Q1', a: 50, b: 25},
+    {q: '2011 Q3', a: 50, b: 25},
+    {q: '2011 Q4', a: 75, b: 50},
+    {q: '2012 Q2', a: 100, b: 75}
+  ],
+  xkey: 'q',
+  ykeys: ['a', 'b'],
+  labels: ['Series A', 'Series B'],
+  lineColors: ['#167f39','#044c29'],
+  lineWidth: 2
+});
 });
